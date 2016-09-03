1799536bb588a41bfac8198e6e5160aa723e6aa6
PutinGit <ein10oira@yahoo.co.jp>
2016-09-03 10:59:42 +0900

    Title Scene

---
+++ Assets/Scripts/TitleTap.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class TitleTap : MonoBehaviour {
+
+	 void Update()
+    {
+        if (MultiTouch.GetTouch() == TouchInfo.Began)//ワンクリックまたはタッチした場合
+        {
+			SceneManager.LoadScene ("Main");
+		}
+	}
+	
+}
