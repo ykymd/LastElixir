5ae7559389201d6fce6b05d63ee68bdbb95bbc4b
PutinGit <ein10oira@yahoo.co.jp>
2016-09-03 13:55:33 +0900

    CandyTotyuu2

--- Assets/Scripts/CandyGenerator.cs
+++ Assets/Scripts/CandyGenerator.cs
@@ -6,13 +6,19 @@ public class CandyGenerator : MonoBehaviour {
 
 	[SerializeField]
 	GameObject refObj;
-	GameManager gamemanager;
+
+	[SerializeField]
+	private GameObject candy;//画像
+
+	GameManager gamemanager;//リストの中のブロックオブジェクトを参照するため
+	private GameObject candies;//candyオブジェクトの親
 
 	// Use this for initialization
 	void Start () {
 		gamemanager = refObj.GetComponent<GameManager> ();
+		candies = new GameObject("Candies");//candyオブジェクトの親を作る、一括で管理しやすくなる
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space)){//スペースキー押したら
@@ -22,10 +28,14 @@ public class CandyGenerator : MonoBehaviour {
 
 	private void GetBlockPosition()
 	{
-		GameObject block = gamemanager.GetList(); //先頭の要素をオブジェクトに保存
-		Vector2 position = block.transform.position;
-		Debug.Log (position.x);
-	
+		GameObject block = gamemanager.GetList(); //先頭のブロック要素をオブジェクトに保存
+		Vector2 position = block.transform.position;//そのブロックの座標を取ってくる
+
+		var obj = Utility.Instantiate(candies, candy);//親の指定と画像指定
+		obj.GetComponent<Rigidbody2D>().Pause(obj.gameObject);
+		obj.transform.position = position;
+		//Debug.Log (position.x);
+
 	}
 
 }
