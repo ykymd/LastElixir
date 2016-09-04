359a9a845e15fcb488c84f73af5dc0959fd84e9e
PutinGit <ein10oira@yahoo.co.jp>
2016-09-04 15:33:16 +0900

    BoomZenin

--- Assets/Scripts/BoomManager.cs
+++ Assets/Scripts/BoomManager.cs
@@ -25,16 +25,23 @@ public class BoomManager : MonoBehaviour {
 
     private void StartBoom()
     {
-        GameObject block = gamemanager.GetAndRemoveList(); //最後尾のブロック要素をオブジェクトに保存
-        Vector2 position = block.transform.position;//そのブロックの座標を取ってくる
-        Destroy(block);//ブロックは消す
-        //配置する座標を設定
-        Vector3 placePosition = position;
-        //配置する回転角を設定
-        Quaternion q = new Quaternion();
-        q= Quaternion.identity;
-        //配置
-        var obj = Utility.Instantiate(boomPrefab,placePosition,q);
-        Destroy(obj, 1.5F);
+        int ListSize = gamemanager.ReturnListSize(); //要素数を保存
+        GameObject block;
+
+        for (int i = ListSize - 1; i >= 0; i--)
+        {
+            block = gamemanager.GetAndRemoveList(); //最後尾のブロック要素をオブジェクトに保存
+            Vector2 position = block.transform.position;//そのブロックの座標を取ってくる
+            Destroy(block);//ブロックは消す
+            //配置する座標を設定
+            Vector3 placePosition = position;
+            //配置する回転角を設定
+            Quaternion q = new Quaternion();
+            q= Quaternion.identity;
+            //配置
+            var obj = Utility.Instantiate(boomPrefab,placePosition,q);
+            Destroy(obj, 1.5F);
+        }
+
     }
 }
