d439d8c765225eec30fb11a1cea33a3951f61a9b
yuki <y.yamada6426@gmail.com>
2016-09-04 22:58:47 +0900

    リザルト画面で詳細表示

---
+++ Assets/ResultBoard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResultBoard : MonoBehaviour
+{
+    [SerializeField]
+    private Text Height = null;
+    [SerializeField]
+    private Text Power = null;
+    [SerializeField]
+    private Text NextRank = null;
+
+    public void SetScore(int height, int rank, int next)
+    {
+        Height.text = string.Format("{0}m", height);
+        Power.text = string.Format("{0}級", rank);
+        NextRank.text = string.Format("{0}m", next);
+    }
+}
