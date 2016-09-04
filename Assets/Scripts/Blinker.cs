e6e96b6747ab8285b4269dfdd57b304223e2876e
yuki <y.yamada6426@gmail.com>
2016-09-05 00:28:03 +0900

    タイトル画面変更

--- Assets/Scripts/Blinker.cs
+++ Assets/Scripts/Blinker.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+
+public class Blinker : MonoBehaviour
+{
+    private float nextTime;
+    public float interval = 1.0f;
+    // 点滅周期
  
-public class Blinker : MonoBehaviour {
-	private float nextTime;
-	public float interval = 1.0f;	// 点滅周期
+    // Use this for initialization
+    void Start()
+    {
+        nextTime = Time.time;
+    }
  
-	// Use this for initialization
-	void Start() {
-		nextTime = Time.time;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextTime)
+        {
+            GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
  
-	// Update is called once per frame
-	void Update() {
-		if ( Time.time > nextTime ) {
-			GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
- 
-			nextTime += interval;
-		}
-	}
+            nextTime += interval;
+        }
+    }
 }
