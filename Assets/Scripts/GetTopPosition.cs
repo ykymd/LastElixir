91918ef3c0ac7bc9fc360ae4917f8f7ddb847f0e
yuki <y.yamada6426@gmail.com>
2016-09-04 17:35:47 +0900

    ゲームオーバーを検出できるようにした

--- Assets/Scripts/GetTopPosition.cs
+++ Assets/Scripts/GetTopPosition.cs
@@ -3,24 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class GetTopPosition : MonoBehaviour {
+public class GetTopPosition : MonoBehaviour
+{
+    private bool isTouched = true;
 
-    public Action CollisionAction;
+    public Action<GameObject> CollisionAction;
 
-	// Use this for initialization
-	void Start () {
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 	
-	}
+    }
 
     //オブジェクトが衝突したとき
-    void OnCollisionEnter2D(Collision2D collision) {
-        if (CollisionAction != null)
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isTouched && collision.gameObject.tag == "Zombie" && CollisionAction != null)
         {
-            CollisionAction();
+            CollisionAction(this.gameObject);
+            isTouched = false;
         }
     }
 }
